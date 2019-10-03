64312a7f65cad380dc9e304749e34c241dd623ec
kaskad88 <kaskad.8888@gmail.com>
2019-10-03 18:07:43 +0300

    UPD: functions.php

--- functions.php
+++ functions.php
@@ -184,17 +184,21 @@ add_action( 'widgets_init', 'azeria_widgets_init' );
  */
 function azeria_assets() {
 
+	$template      = get_template();
+	$theme_obj     = wp_get_theme( $template );
+	$theme_version = $theme_obj->get( 'Version' );
+
 	$base_url = get_template_directory_uri();
 
 	// Styles
 	wp_enqueue_style( 'azeria-fonts', azeria_fonts_url() );
-	wp_enqueue_style( 'azeria-style', get_stylesheet_uri(), false, '1.1.0' );
+	wp_enqueue_style( 'azeria-style', get_stylesheet_uri(), false, $theme_version );
 
 	// Scripts
 	wp_enqueue_script( 'jquery-slick', $base_url . '/js/slick.min.js', array( 'jquery' ), '1.8.1', true );
 	wp_enqueue_script( 'jquery-magnific-popup', $base_url . '/js/jquery.magnific-popup.min.js', array( 'jquery' ), '1.1.0', true );
-	wp_enqueue_script( 'azeria-navigation', $base_url . '/js/navigation.js', array( 'jquery', 'hoverIntent' ), '20120206', true );
-	wp_enqueue_script( 'azeria-skip-link-focus-fix', $base_url . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'azeria-navigation', $base_url . '/js/navigation.js', array( 'jquery', 'hoverIntent' ), $theme_version, true );
+	wp_enqueue_script( 'azeria-skip-link-focus-fix', $base_url . '/js/skip-link-focus-fix.js', array(), $theme_version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -206,7 +210,7 @@ function azeria_assets() {
 		wp_enqueue_script( 'azeria-stickup', $base_url . '/js/jquery.stickup.js', array( 'jquery' ), '1.1.0', true );
 	}
 
-	wp_enqueue_script( 'azeria-custom-script', $base_url . '/js/script.js', array( 'jquery' ), '1.1.0', true );
+	wp_enqueue_script( 'azeria-custom-script', $base_url . '/js/script.js', array( 'jquery' ), $theme_version, true );
 }
 add_action( 'wp_enqueue_scripts', 'azeria_assets' );
 
