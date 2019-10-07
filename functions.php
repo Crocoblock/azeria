0e84b0dba676de753b2c979503262f9c862c37b0
kaskad88 <kaskad.8888@gmail.com>
2019-10-07 16:23:22 +0300

    Added: Gutenberg compatibility

--- functions.php
+++ functions.php
@@ -129,6 +129,21 @@ function azeria_setup() {
 
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	// Add support for Block Styles.
+	add_theme_support( 'wp-block-styles' );
+
+	// Add support for editor styles.
+	add_theme_support( 'editor-styles' );
+
+	// Enqueue editor styles.
+	add_editor_style( 'style-editor.css' );
+
+	// Add support for full and wide align images.
+	add_theme_support( 'align-wide' );
+
+	// Add support for responsive embedded content.
+	add_theme_support( 'responsive-embeds' );
 }
 endif; // azeria_setup
 add_action( 'after_setup_theme', 'azeria_setup' );
